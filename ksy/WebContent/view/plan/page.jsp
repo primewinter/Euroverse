406b79cf59ed4b0e705127f783216c068d4f80a2
User <User@DESKTOP-48M42UR>
2020-01-31 17:50:37 +0900

    obj : (1/31 금) 집가기 전 커밋

--- ksy/WebContent/view/plan/page.jsp
+++ ksy/WebContent/view/plan/page.jsp
@@ -4,9 +4,26 @@
 <html>
 <head>
 <meta charset="EUC-KR">
-<title>Insert title here</title>
+<title>Plan test page</title>
+	<style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 500px;
+        width: 50%;
+        background-color: grey;
+      }
+      /* Optional: Makes the sample page fill the window. */
+    </style>
+
+
+
 </head>
 <body>
+
+
+블라블라
+
 
 </body>
 </html>
--- ksy/src/main/java/com/ksy/web/plan/PlanController.java
+++ ksy/src/main/java/com/ksy/web/plan/PlanController.java
@@ -1,12 +1,14 @@
 package com.ksy.web.plan;
 
-import java.util.Calendar;
 import java.util.List;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,11 +40,15 @@ public class PlanController {
 	}
 	
 	@RequestMapping( value = "getPlanList", method = RequestMethod.GET )
-	public String getPlanList (	@RequestParam("userId") String userId, Model model	) throws Exception {
+	public String getPlanList (	@RequestParam("userId") String userId, Model model, HttpSession session	) throws Exception {
 		
-		userId = "admin";
+		User user = (User)session.getAttribute("user");
+		//test용 if문 : 회원아이디 셋팅
+		if(user == null) {
+			user.setUserId("admin");
+		}
 		
-		List<Plan> listPlan = planService.getPlanList(userId);
+		List<Plan> listPlan = planService.getPlanList(user.getUserId());
 		
 		model.addAttribute("list", listPlan);
 		
@@ -69,9 +75,6 @@ public class PlanController {
 		plan.setMemoList(memoList);
 		
 		
-
-		
-		
 		
 		plan.setPlanDday(0);		//여행 D-Day
 		plan.setEndDate("2020-11-11");	//여행종료일자
@@ -86,6 +89,53 @@ public class PlanController {
 		model.addAttribute("plan", plan);
 		
 		return "forward:/plan/getPlan.jsp";
+	}
+	
+	
+	@RequestMapping( value = "addPlan", method = RequestMethod.POST )
+	public String addPlan (	@ModelAttribute("plan") Plan plan, Model model, HttpSession session	) throws Exception {
+		
+		planService.addPlan(plan);	
+		
+		User user = (User)session.getAttribute("user");		//음..
+		
+		List<Plan> listPlan = planService.getPlanList(plan.getPlanMaster().getUserId());
+		//혹은..... => planService.getPlanList(user.getUserId());
+		
+		model.addAttribute("list", listPlan);
+		
+		return "forward:/plan/getPlanList.jsp";
+	}
+	
+	
+	@RequestMapping( value = "updatePlan", method = RequestMethod.POST )
+	public String updatePlan (	@ModelAttribute("plan") Plan plan, Model model	) throws Exception {
+	
+		planService.updatePlan(plan);
+		
+		//plan = planService.getPlan(plan.getPlanId());	<- 이 과정이 굳이 필요한가..?
+		//model.addAttribute("plan", plan);	
+		
+		return "forward:/plan/getPlan.jsp";
+	}
+	
+	@RequestMapping( value = "updatePlanStatus", method = RequestMethod.POST )
+	public String updatePlanStatus (	@ModelAttribute("plan") Plan plan, Model model	) throws Exception {
+	
+		planService.updatePlanStatus(plan);
+	
+		//plan = planService.getPlan(plan.getPlanId());	<- 이 과정이 굳이 필요한가..?
+		//model.addAttribute("plan", plan);		
+		
+		return "forward:/plan/getPlan.jsp";
+	}
+	
+	@RequestMapping( value = "deletePlan", method = RequestMethod.POST )
+	public String deletePlan (	@RequestParam("planId") String planId, Model model	) throws Exception {
+	
+		planService.deletePlan(planId);
+		
+		return "forward:/plan/getPlanList.jsp";
 	}
 	
 	
