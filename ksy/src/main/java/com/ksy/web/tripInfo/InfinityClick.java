ea1cb2f16a4a24c1b43093d4732f57239a69f24e
User <User@DESKTOP-R3H9U8R>
2020-02-10 09:30:39 +0900

    winning : (2/10 월) 여행정보조회 수정

--- ksy/src/main/java/com/ksy/web/tripInfo/InfinityClick.java
+++ ksy/src/main/java/com/ksy/web/tripInfo/InfinityClick.java
@@ -10,14 +10,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.internal.WebElementToJsonConverter;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class InfinityClick {
 	
 	public static void main(String[] args) throws Exception {
-
-		InfinityClick selTest = new InfinityClick();
-		selTest.imgCrawl();
-		selTest.infoCrawl();
+		
+		System.out.println("main 메소드 실행???");
+		//페이지마다 크롤링을 하기위해 for문 작동 
+//		for(int i=1; i<2; i++) {
+			InfinityClick selTest = new InfinityClick();
+			selTest.imgCrawl();
+			selTest.infoCrawl();
+//		}
 	}
 
 	// WebDriver
@@ -29,9 +35,9 @@ public class InfinityClick {
 	public static final String WEB_DRIVER_PATH = "C:\\chromedriver.exe";
 
 	private String base_url;
-
+	
+	//int값을 넘겨받아서 한페이지 한페이지 크롤링을 할수있음
 	public InfinityClick() {
-		super();
 
 		// System Property SetUp
 		System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
@@ -41,26 +47,28 @@ public class InfinityClick {
 		String dreamCity = "유럽";
 		
 		// pixabay 홈페이지에서 크롤링 검색정보는 다이나믹하게
-		base_url = "https://pixabay.com/ko/images/search/" + dreamCity + "/?cat=buildings"; 
+		base_url = "https://pixabay.com/ko/images/search/" + dreamCity + "/?cat=buildings";
+		//&pagi=+String.valueOf(no); 
 	
-		
-		
-
 	}
 
 	public Map<Integer,String> imgCrawl() throws Exception {
 
 
 		System.out.println("crawling start!!!!");
+		
 		//크롤링 해올 url 가져오기
 		driver.get(base_url);
 		driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/div/label/input")).click();
+//		WebDriverWait wait = new WebDriverWait(driver,10000); 
 		
 		//selenium에 제공되는 By Class를 이용하여 CssSeletor로 Css접근 
 		By picture = By.cssSelector("div.flex_grid.credits.search_results > div > a > img");
 		
 		//seletor로 접근한 tag를 webElement형태로 list에 저장.
 		List<WebElement> pictures = driver.findElements(picture);
+		
+//				wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(picture)); // 화면 로딩시간을 기다려주는 메소드
 		System.out.println("pictures ==>" + pictures);
 		
 		//디버깅
